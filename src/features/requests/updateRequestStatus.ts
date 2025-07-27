0045a238c5d38875e7d4f2071c33fddee87fdbce
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-07-27 14:33:16 +0100

    upadte to post

--- src/features/requests/updateRequestStatus.ts
+++ src/features/requests/updateRequestStatus.ts
@@ -2,7 +2,7 @@ import { overseerrApi } from "@/lib/overseerr";
 import { UpdateRequestStatusResponse } from './types';
 
 export const updateRequestStatus = async (requestId: number, status: "approved" | "rejected"): Promise<UpdateRequestStatusResponse> => {
-    const response = await overseerrApi.patch(`/request/${requestId}/status`, {
+    const response = await overseerrApi.post(`/request/${requestId}/status`, {
         status: status
     })
     return response.data
