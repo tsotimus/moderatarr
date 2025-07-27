4ee9cd07a75185cffe7de815279fdc34bc901aca
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-07-27 12:51:05 +0100

    update seasons getter

--- src/features/media/handleTv.ts
+++ src/features/media/handleTv.ts
@@ -1,13 +1,14 @@
 import { OverseerrWebhookPayload } from "@/lib/overseerr";
 import { GetRequestResponse } from "../requests/types";
 
-const getSeasons = (payload: OverseerrWebhookPayload): number | null => {
+const getHowManySeasons = (payload: OverseerrWebhookPayload): number | null => {
     const extras = payload.extra
     if(extras) {
         const requestedSeasons = extras.find(extra => extra.name === "Requested Seasons")
         if(requestedSeasons){
             try {
-                return parseInt(requestedSeasons.value)
+                const requestedSeasonsArray = requestedSeasons.value.split(",")
+                return requestedSeasonsArray.length
             } catch (error) {
                 console.error(`Error parsing requested seasons ${requestedSeasons.value}`, error)
                 return null
@@ -19,7 +20,7 @@ const getSeasons = (payload: OverseerrWebhookPayload): number | null => {
 
 export const handleTVAnime = async (request: GetRequestResponse, payload: OverseerrWebhookPayload) => {
     console.log(payload)
-    const requestedSeasons = getSeasons(payload)
+    const requestedSeasons = getHowManySeasons(payload)
     console.log("requestedSeasons", requestedSeasons)
   return true;
 };
@@ -27,7 +28,7 @@ export const handleTVAnime = async (request: GetRequestResponse, payload: Overse
 export const handleTVNonAnime = async (request: GetRequestResponse, payload: OverseerrWebhookPayload) => {
     //Check how many seasons are in the request and how many there are in total
     console.log(payload)
-    const requestedSeasons = getSeasons(payload)
+    const requestedSeasons = getHowManySeasons(payload)
     console.log("requestedSeasons", requestedSeasons)
   return true;
 };
