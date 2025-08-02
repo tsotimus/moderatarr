c0ee19f55d27039718a46dfad6295cb9864965ac
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-08-03 00:43:58 +0100

    dont import vars into drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
-import { env } from './src/env';
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/lib/db/schema',
   dialect: 'sqlite',
   dbCredentials: {
-    url: env.DB_FILE_NAME,
+    url: process.env.DB_FILE_NAME || 'moderatarr.db',
   },
 });
