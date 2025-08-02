ad364de229960e2f275e48b4c5f782f884ad2a3e
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-08-03 00:33:56 +0100

    update migrations

--- drizzle/0002_natural_triton.sql
+++ drizzle/0002_natural_triton.sql
@@ -2,12 +2,12 @@ PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `__new_profiles` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
-	`profileId` integer NOT NULL,
+	`profileId` integer NOT NULL DEFAULT 0,
 	`serverId` integer NOT NULL,
 	`serverType` text NOT NULL
 );
 --> statement-breakpoint
-INSERT INTO `__new_profiles`("id", "name", "profileId", "serverId", "serverType") SELECT "id", "name", "profileId", "serverId", "serverType" FROM `profiles`;--> statement-breakpoint
+INSERT INTO `__new_profiles`("id", "name", "serverId", "serverType") SELECT "id", "name", "serverId", "serverType" FROM `profiles`;--> statement-breakpoint
 DROP TABLE `profiles`;--> statement-breakpoint
 ALTER TABLE `__new_profiles` RENAME TO `profiles`;--> statement-breakpoint
 PRAGMA foreign_keys=ON;
